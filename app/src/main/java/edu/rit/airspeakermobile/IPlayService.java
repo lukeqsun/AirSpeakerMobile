e0013c3d559bde23e95475162fa0f3eb24931042
Qiyun Sun <luke.q.sun@gmail.com>
2015-04-30 15:05:58 -0400

    Import projects to Android Studio

---
+++ app/src/main/java/edu/rit/airspeakermobile/IPlayService.java
@@ -0,0 +1,12 @@
+package edu.rit.airspeakermobile;
+
+/*
+port from fenggit's package com.example.airplay;
+ */
+
+public interface IPlayService {
+
+    //public void stop();
+
+    public TransportState getPlayerState();
+}
